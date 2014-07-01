141e20e6af7d1d34f90a1145ae512d537c978867
Ahmed Sami <wenning9ahmed@hotmail.com>
2014-07-01 09:41:05 +0200

    Media Added.

--- src/com/asyn/jamesbond/SplashActivity.java
+++ src/com/asyn/jamesbond/SplashActivity.java
@@ -1,26 +1,41 @@
 package com.asyn.jamesbond;
 
+import com.asyn.jamesbond.media.RawPlayer;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashActivity extends Activity {
-	
-	public static final int SPLASH_LENGTH = 1 * 1000;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_activity);
-        
-        new Handler().postDelayed(new Runnable() {			
+	public static final int SPLASH_LENGTH = 1 * 1000;
+	public static final int INTRO_MEDIA = R.raw.splash_intro_loud;
+
+	protected RawPlayer mediaPlayer;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash_activity);
+
+		mediaPlayer = new RawPlayer(this, INTRO_MEDIA);
+		mediaPlayer.startPlayableMedia();
+
+		new Handler().postDelayed(new Runnable() {
 			@Override
 			public void run() {
-				startActivity(new Intent(SplashActivity.this, MainActivity.class));
+				startActivity(new Intent(SplashActivity.this,
+						MainActivity.class));
 				finish();
 			}
-		}, SPLASH_LENGTH);
-        
-    }
+		}, mediaPlayer.getFileDuration());
+
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		mediaPlayer.destroy();
+	}
 }
