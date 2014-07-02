2cd4de1031228fae9b1873cc8afec709093278b5
Ahmed Sami <wenning9ahmed@hotmail.com>
2014-07-02 07:30:03 +0200

    Splash Loading Animation...

--- src/com/asyn/jamesbond/SplashActivity.java
+++ src/com/asyn/jamesbond/SplashActivity.java
@@ -1,26 +1,41 @@
 package com.asyn.jamesbond;
 
-import com.asyn.jamesbond.media.RawPlayer;
-
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
+
+import com.asyn.jamesbond.media.RawPlayer;
 
 public class SplashActivity extends Activity {
 
 	public static final int SPLASH_LENGTH = 1 * 1000;
 	public static final int INTRO_MEDIA = R.raw.splash_intro;
 
+	protected ImageView animationLayer;
 	protected RawPlayer mediaPlayer;
+	private AnimationDrawable splashAnimation;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash_activity);
 
+		animationLayer = (ImageView) findViewById(R.id.animationImageView);
+		animationLayer.setBackgroundResource(R.drawable.splash_animation);
+		splashAnimation = (AnimationDrawable) animationLayer.getBackground();
 		mediaPlayer = new RawPlayer(this, INTRO_MEDIA);
+
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+
 		mediaPlayer.startPlayableMedia();
+		splashAnimation.start();
 
 		new Handler().postDelayed(new Runnable() {
 			@Override
@@ -32,7 +47,7 @@ public class SplashActivity extends Activity {
 		}, mediaPlayer.getFileDuration());
 
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
