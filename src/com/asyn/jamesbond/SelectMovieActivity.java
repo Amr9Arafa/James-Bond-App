03aeb918a94014743f08175c6810f91b6e535a4a
Ahmed Sami <wenning9ahmed@hotmail.com>
2014-07-05 13:29:08 +0200

    FIXED!

--- src/com/asyn/jamesbond/SelectMovieActivity.java
+++ src/com/asyn/jamesbond/SelectMovieActivity.java
@@ -3,6 +3,7 @@ package com.asyn.jamesbond;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -119,7 +120,7 @@ public class SelectMovieActivity extends Activity {
 	@Override
 	protected void onResume() {
 		super.onResume();
-		
+		type = getIntent().getIntExtra(Keys.TYPE, -1);
 	}
 
 }
